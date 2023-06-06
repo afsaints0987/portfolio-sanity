6b1e8ccc8bc6339368f8dc896e387ad950337af4
Aaron Fritz Santos <Aaron.Santos@EntainGroup.com>
2023-06-06 16:29:06 +0800

    add project.js

---
+++ schemas/project.js
@@ -0,0 +1,64 @@
+export default {
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+    },
+    {
+      name: 'date',
+      type: 'datetime',
+    },
+    {
+      name: 'description',
+      type: 'text',
+    },
+    {
+      name: 'mainImage',
+      title: 'Main Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'projectType',
+      title: 'Project type',
+      type: 'string',
+      options: {
+        list: [
+          {
+            value: 'personal',
+            title: 'Personal',
+          },
+          {
+            value: 'client',
+            title: 'Client',
+          },
+          {
+            value: 'capstone',
+            title: 'Capstone',
+          },
+        ],
+      },
+    },
+    {
+      name: 'link',
+      type: 'url',
+    },
+    {
+      name: 'tags',
+      type: 'array',
+      of: [
+        {
+          type: 'string',
+        },
+      ],
+      options: {
+        layout: 'tags',
+      },
+    },
+  ],
+}
